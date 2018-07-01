eeb7956bdb8d615149ce4bdec8af83dda97a21b8
ralphhm <ralphhm+github@gmail.com>
2018-07-02 00:11:49 +0200

    Implement list items for error and loading state

--- app/src/main/java/de/rhm/gitbrowse/BrowseActivity.kt
+++ app/src/main/java/de/rhm/gitbrowse/BrowseActivity.kt
@@ -2,12 +2,10 @@ package de.rhm.gitbrowse
 
 import android.arch.lifecycle.Observer
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Section
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
-import de.rhm.gitbrowse.api.RepositoryItem
 import kotlinx.android.synthetic.main.activity_browse.*
 import kotlinx.android.synthetic.main.content_browse.*
 import org.koin.android.architecture.ext.viewModel
@@ -26,9 +24,8 @@ class BrowseActivity : AppCompatActivity() {
     }
 
     private fun bind(state: BrowseUiState) = when (state) {
-        BrowseUiState.Loading -> Snackbar.make(toolbar, "Loading", Snackbar.LENGTH_LONG).show()
-        is BrowseUiState.Failure -> Snackbar.make(toolbar, state.reason
-                ?: "Unknown error", Snackbar.LENGTH_LONG).show()
+        BrowseUiState.Loading -> section.update(listOf(LoadingItem))
+        is BrowseUiState.Failure -> section.update(listOf(ErrorItem(state.reason)))
         is BrowseUiState.Success -> section.update(state.repositories.map { RepositoryItem(it) })
     }
 
--- app/src/main/java/de/rhm/gitbrowse/ListItems.kt
+++ app/src/main/java/de/rhm/gitbrowse/ListItems.kt
@@ -1,10 +1,15 @@
-package de.rhm.gitbrowse.api
+package de.rhm.gitbrowse
 
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
-import de.rhm.gitbrowse.R
 import de.rhm.gitbrowse.api.model.Repository
+import kotlinx.android.synthetic.main.item_error.*
 import kotlinx.android.synthetic.main.item_repository.*
+
+object LoadingItem : Item() {
+    override fun bind(viewHolder: ViewHolder, position: Int) = Unit
+    override fun getLayout() = R.layout.item_loading
+}
 
 class RepositoryItem(val repository: Repository) : Item() {
 
@@ -16,4 +21,13 @@ class RepositoryItem(val repository: Repository) : Item() {
     }
 
     override fun getLayout() = R.layout.item_repository
+}
+
+class ErrorItem(val cause: String?) : Item() {
+
+    override fun bind(viewHolder: ViewHolder, position: Int) = with(viewHolder) {
+        error_info.text = itemView.resources.getString(R.string.error_loading_repos, cause)
+    }
+
+    override fun getLayout() = R.layout.item_error
 }
